327bf5582877bb62f78364abd19386f63463134f
wspwebben <wspwebben@gmail.com>
2021-01-10 00:45:03 +0700

    fix awards

--- template-parts/section-client.php
+++ template-parts/section-client.php
@@ -1,38 +1,39 @@
 <?php
 /**
- * Section Client
+ * Section Client (Awards)
  */
- $construction_lite_client_cat = get_theme_mod('construction_lite_client_cat');
- if($construction_lite_client_cat):
-    $construction_lite_client_args = array(
-        'post_type' => 'post',
+
+?>
+ 
+<?php 
+    $client_args = array(
+        'post_type' => 'award',
         'order' => 'DESC',
         'posts_per_page' => -1,
         'post_status' => 'publish',
-        'category_name' => $construction_lite_client_cat
     );
-    $construction_lite_client_query = new WP_Query($construction_lite_client_args);
-  if($construction_lite_client_query->have_posts()):
+
+    $client_query = new WP_Query($client_args);
+    
+    if($client_query->have_posts()):
 ?>
-
     <section class="awards">
-    <div class="awards__container ak-container">
-        <ul class="awards__list">
-            <?php
-                while($construction_lite_client_query->have_posts()):
-                    $construction_lite_client_query->the_post();
-                    $construction_lite_client_logo_src = wp_get_attachment_image_src(get_post_thumbnail_id(),'construction-client-logo');
-                    $construction_lite_client_logo_url = $construction_lite_client_logo_src[0];
-                ?>
+        <div class="awards__container ak-container">
+            <ul class="awards__list">
+                <?php
+                    while($client_query->have_posts()):
+                        $client_query->the_post();
+                        $client_logo_src = wp_get_attachment_image_src(get_post_thumbnail_id(),'construction-client-logo');
+                        $client_logo_url = $client_logo_src[0];
+                    ?>
 
-                <?php if($construction_lite_client_logo_url): ?>
-                    <li class="awards__item wow fadeInUp">
-                        <img class="awards__image" src="<?php echo esc_url($construction_lite_client_logo_url); ?>" alt="<?php the_title_attribute() ?>" />
-                    </li>
-                <?php endif; ?>
-            <?php endwhile; wp_reset_postdata(); ?>
-        </ul>
-    </div>
-    <?php
-  endif;
-endif;
+                    <?php if($client_logo_url): ?>
+                        <li class="awards__item wow fadeInUp">
+                            <img class="awards__image" src="<?php echo esc_url($client_logo_url); ?>" alt="<?php the_title_attribute() ?>" />
+                        </li>
+                    <?php endif; ?>
+                <?php endwhile; wp_reset_postdata(); ?>
+            </ul>
+        </div>
+    </section>
+<?php endif; ?>
