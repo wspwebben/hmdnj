aa550c5280b7f622bc51e5993077f88af22a0bab
wspwebben <wspwebben@gmail.com>
2021-01-10 00:12:59 +0700

    fix services

--- template-parts/section-feature.php
+++ template-parts/section-feature.php
@@ -2,54 +2,53 @@
 /**
  * Feature Section
  */
- $construciton_feature_section_title = get_theme_mod('construction_lite_feature_title');
- $construciton_feature_section_sub_title = get_theme_mod('construction_lite_feature_sub_title');
- $construction_lite_feature_section_image = get_theme_mod('construction_lite_feature_image');
- ?>
+
+?>
  
-<?php if($construciton_feature_section_title || $construciton_feature_section_sub_title){ ?>
-    <div class="section-title-sub-wrap wow fadeInUp">
-        <div class="ak-container">
-            <?php if($construciton_feature_section_title){ ?><div class="section-title"><h5><?php echo esc_html($construciton_feature_section_title); ?></h5></div><?php } ?>
-            <?php if($construciton_feature_section_sub_title) { ?><div class="section-sub-title"><h2><?php echo esc_html($construciton_feature_section_sub_title); ?></h2></div><?php } ?>
-        </div>
-    </div>
-<?php } ?>
+<div class="section-title-sub-wrap wow fadeInUp">
+	<div class="ak-container">
+		<div class="section-title"><h5>WHAT WE OFFER</h5>
+		<div class="section-sub-title"><h2>OUR SERVICES</h2></div>
+	</div>
+</div>
+
 <?php
-        wp_reset_postdata();
-        $construction_lite_feature_args = array(
-            'post_type' => 'service',
-            'order' => 'DESC',
-            'posts_per_page' => -1,
-            'post_status' => 'publish',
-        );
-        $construction_lite_feature_query = new WP_Query($construction_lite_feature_args);
-        if($construction_lite_feature_query->have_posts() || $construction_lite_feature_section_image):
-        ?>
-            <div class="wow fadeInUp features">
-			  <div class="ak-container">
-				<ul class="features__list">
-				  <?php
-					while($construction_lite_feature_query->have_posts()): $construction_lite_feature_query->the_post();
-					$construction_lite_feature_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(),'construction-feature-image')[0];
-				  ?>
+	wp_reset_postdata();
+	$feature_args = array(
+			'post_type' => 'service',
+			'order' => 'DESC',
+			'posts_per_page' => -1,
+			'post_status' => 'publish',
+	);
+	$feature_query = new WP_Query($feature_args);
+	
+	if($feature_query->have_posts()):
+?>
+	<div class="wow fadeInUp features">
+		<div class="ak-container">
+			<ul class="features__list">
+				<?php while($feature_query->have_posts()):
+								$feature_query->the_post();
+								$feature_image_src = wp_get_attachment_image_src(get_post_thumbnail_id(),'construction-feature-image');
+								$feature_image_url = $feature_image_src[0];
+				?>
 					<li class="features__item">
 					  <article class="features__wrapper">
-						<img src="<?php echo esc_url($construction_lite_feature_image_url); ?>" alt="" />
-						<h3 class="features__title">
-						  <a class="features__link" href="<?php the_permalink(); ?>">
-							<?php the_title(); ?>
-						  </a>
-						</h3>
-						<div class="feature__content">
-						  <?php echo esc_html(wp_trim_words(get_the_content(),'15','...')); ?>
-						</div>
+							<img src="<?php echo esc_url($feature_image_url); ?>" alt="" />
+							<h3 class="features__title">
+								<a class="features__link" href="<?php the_permalink(); ?>">
+									<?php the_title(); ?>
+								</a>
+							</h3>
+							<div class="feature__content">
+								<?php echo esc_html(wp_trim_words(get_the_content(),'15','...')); ?>
+							</div>
 
-						<a class="feature__button" href="<?php the_permalink(); ?>">Read more</a>
+							<a class="feature__button" href="<?php the_permalink(); ?>">Read more</a>
 					  </article>
 					</li>
-				  <?php endwhile; ?>
-				</ul>
-			  </div>
-			</div>
-        <?php endif; ?>
+				<?php endwhile; ?>
+			</ul>
+		</div>
+	</div>
+<?php endif; ?>
