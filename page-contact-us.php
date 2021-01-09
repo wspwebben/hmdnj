61c354a05f86145b1342372076d8cd72d2926803
wspwebben <wspwebben@gmail.com>
2021-01-09 17:29:20 +0700

    add templates for contact us and services pages

---
+++ page-contact-us.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying contact us page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package construction lite
+ */
+
+get_header();
+do_action('construction_lite_header_banner');?>
+    <div class="ak-container">
+    	<div id="primary" class="content-area">
+    		<main id="main" class="site-main" role="main">
+    
+    			<?php
+    			while ( have_posts() ) : the_post();
+    
+    				get_template_part( 'template-parts/content', 'page' );
+    
+    				// If comments are open or we have at least one comment, load up the comment template.
+    				if ( comments_open() || get_comments_number() ) :
+    					comments_template();
+    				endif;
+    
+    			endwhile; // End of the loop.
+    			?>
+    
+    		</main><!-- #main -->
+    	</div><!-- #primary -->
+    </div>
+<?php
+get_footer();
