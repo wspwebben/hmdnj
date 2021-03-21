e192961910d1a664e5e89a410c3fd79cb716a3f8
wspwebben <wspwebben@gmail.com>
2021-03-22 00:28:54 +0700

    update projects page

--- page-projects.php
+++ page-projects.php
@@ -7,23 +7,23 @@
  * @package construction lite
  */
 
-get_header();
-do_action('header_banner');?>
+$portfolio_args = array(
+  'post_type' => 'project',
+  'order' => 'DESC',
+  'posts_per_page' => -1,
+  'post_status' => 'publish',
+);
+$portfolio_query = new WP_Query($portfolio_args);
+
+get_header();?>
     <div class="ak-container">
-    	<div id="primary" class="content-area">
-    		<main id="main" class="site-main" role="main">
-    
-    			<?php
-            $portfolio_args = array(
-              'post_type' => 'project',
-              'order' => 'DESC',
-              'posts_per_page' => -1,
-              'post_status' => 'publish',
-            );
-            $portfolio_query = new WP_Query($portfolio_args);
+    	<div id="primary">
+    		<main id="main" class="site-main p-t-60" role="main">
+          <div class="section-sub-title">
+						<h2>Our projects</h2>
+					</div>
             
-            if($portfolio_query->have_posts()):
-          ?>
+          <?php if($portfolio_query->have_posts()): ?>
             <ul class="portfolio">
               <?php
                 while($portfolio_query->have_posts()):
@@ -32,23 +32,26 @@ do_action('header_banner');?>
                       $portfolio_image_url = $portfolio_image_src[0]; 
               ?>
                 <li class="portfolio__item">
-                  <article class="portfolio__wrapper portfolio-item" style="background-image: url(<?php echo esc_url($portfolio_image_url) ?>)">
+                  <article class="portfolio-item" style="background-image: url(<?php echo esc_url($portfolio_image_url) ?>)">
                     <h3 class="portfolio-item__title">
                       <a class="portfolio-item__link" href="<?php the_permalink() ?>">
-                        <?php the_title(); ?>
+                        <span><?php the_title(); ?></span>
                       </a>
                     </h3>
                   </article>
                 </li>
               <?php endwhile;?>
+              
+              <li class="portfolio__item portfolio__item--form">
+                <?php
+                  echo do_shortcode( '[wpforms id="197"]' );
+                ?>
+              </li>
             </ul>
             <?php wp_reset_postdata(); ?>
           <?php endif; ?>
     
     		</main><!-- #main -->
     	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-?> </div> <?php
+    </div> <?php
 get_footer();
